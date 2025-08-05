79e47b21ed9c51d54fa0194cc05be99eb91ff8ca
김지윤 <dpdupplexx9@gmail.com>
2025-08-05 23:33:17 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+long long int fac(long long int n) {
+	if (n == 0)return 1;
+	return n * fac(n - 1);
+}
+int main() {
+	long long int n;
+	scanf("%lld", &n);
+	printf("%lld", fac(n));
+	return 0;
+}
