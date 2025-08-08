60806f0927b77ceb9d1b3f7c365710d410593789
김지윤 <dpdupplexx9@gmail.com>
2025-08-08 16:04:14 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int n, i, sum = 0;
+	for (i = 0; i < 5; i++) {
+		scanf("%d", &n);
+		sum += n;
+	}
+	printf("%d", sum);
+	return 0;
+}
