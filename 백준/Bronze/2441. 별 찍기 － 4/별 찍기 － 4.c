0424f7e5cbdf3ff4da0487c39d90004f00e3c13b
김지윤 <dpdupplexx9@gmail.com>
2025-08-23 20:25:59 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int n, i, j;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < i; j++) {
+			printf(" ");
+		}
+		for (j = n - i; j > 0; j--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
