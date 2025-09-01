48208fdb929ff3adb0b9fb0e511adbb64196b5fa
김지윤 <dpdupplexx9@gmail.com>
2025-09-01 19:51:00 +0900

    [Bronze IV] Title: Which Alien?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int a, e;
+	scanf("%d %d", &a, &e);
+	if (a >= 3 && e <= 4) printf("TroyMartian\n");
+	if (a <= 6 && e >= 2)printf("VladSaturnian\n");
+	if (a <= 2 && e <= 3)printf("GraemeMercurian\n");
+	return 0;
+}
