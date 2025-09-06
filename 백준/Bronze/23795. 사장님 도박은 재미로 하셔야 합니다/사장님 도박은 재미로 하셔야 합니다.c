913a1320dff5b8be6a30c23f9af31b12484443df
김지윤 <dpdupplexx9@gmail.com>
2025-09-06 20:42:22 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int a, sum = 0;
+	while (1) {
+		scanf("%d", &a);
+		if (a == -1)break;
+		sum += a;
+	}
+	printf("%d", sum);
+	return 0;
+}
