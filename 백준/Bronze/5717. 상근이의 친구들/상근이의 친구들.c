286663fd526b3ccae49a0a2b8c2d2e2b5a340ff5
김지윤 <dpdupplexx9@gmail.com>
2025-09-16 10:08:13 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int m, f;
+	while (1) {
+		scanf("%d %d", &m, &f);
+		if (m == 0 && f == 0)break;
+		printf("%d\n", m + f);
+	}
+	return 0;
+}
