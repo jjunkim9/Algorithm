9fb5d347c8d8fdecb64fa7544f515eb254492720
김지윤 <jjunkim@dsm.hs.kr>
2025-12-19 14:06:25 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int n, i, j;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		for (j = 1; j < n - i; j++) printf(" ");
+		for (j = 0; j < i * 2 + 1; j++) printf("*");
+		printf("\n");
+	}
+	for (i = 0; i < n - 1; i++) {
+		for (j = 0; j <= i; j++) printf(" ");
+		for (j = i; j < (n - 1) * 2 - i - 1; j++) printf("*");
+		printf("\n");
+	}
+	return 0;
+}
